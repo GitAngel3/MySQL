868150f98358b506e7578cac3471c59f451ccf8a
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-04 10:16:46 +0530

    MySQL

---
+++ MySQL-CW/CW1.sql
@@ -0,0 +1,5 @@
+INSERT INTO `students`(`id`, `name`, `age`, `department`, `grade`) 
+VALUES ('1','William','23','Physics','78'),
+      ('2','Steffy','21','Chemistry','80'),
+      ('3','John','22','Biology','82'),
+      ('4','Angel','23','Computer Science','80');
