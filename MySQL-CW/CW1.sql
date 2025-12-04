f0f73e7d111af6da98d01dc2f775e6012f405493
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-04 10:42:28 +0530

    Mysql

--- MySQL-CW/CW1.sql
+++ MySQL-CW/CW1.sql
@@ -3,3 +3,11 @@ VALUES ('1','William','23','Physics','78'),
       ('2','Steffy','21','Chemistry','80'),
       ('3','John','22','Biology','82'),
       ('4','Angel','23','Computer Science','80');
+
+      SELECT * FROM `students` WHERE age>20;
+
+      SELECT * FROM `students` WHERE department IN('Computer Science','Physics'); 
+
+      SELECT * FROM `students` WHERE grade=90;
+
+      SELECT * FROM `students` WHERE grade BETWEEN 70 AND 90;
