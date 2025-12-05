ae29ec9d2dfbfb947f8a1841f44662ced006350c
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-05 12:01:08 +0530

    hw-mysql

---
+++ MySQL-HW/HW2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `books_store`(`id`, `title`, `author`, `price`, `stock_status`, `genre`) 
+VALUES ('111','Don Quixote','Miguel de Cervantes','250','Out of Stock','comic novel'),
+       ('131','1984','George Orwell','800','In stock','political fiction'),
+       ('142','Pride and Prejudice','Jane Austen','1200','In stock','romantic fiction'),
+       ('161','Alices Adventures in Wonderland','Lewis Carroll','550','Out of Stock','childrens fiction');
