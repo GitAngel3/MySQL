11063beb012c06db5a7706737f38ed713c5631dd
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-08 22:31:31 +0530

    HW

---
+++ MySQL-HW/HW3.sql
@@ -0,0 +1,4 @@
+INSERT INTO `training_student`
+VALUE(1, 'Alice', 'Web Development', 5000, 'Inactive'),
+(2, 'Bob', 'Data Science', 7000, 'Inactive'),
+(3, 'Charlie', 'UI/UX Design', 4000, 'Active');
