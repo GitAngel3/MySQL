d9533349a370fdde7fd76d32be56eb7092f022b9
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-10 21:37:33 +0530

    hw

---
+++ MySQL-HW/CW4.sql
@@ -0,0 +1,14 @@
+INSERT INTO `book_database`(`id`, `title`, `author`, `price`, `stock`) 
+VALUES ('1', 'The Alchemist', 'Paulo Coelho', 350, 50),
+       ('2', 'Atomic Habits', 'James Clear', 450, 40),
+       ('3', 'The Psychology of Money', 'Morgan Housel', 400, 30),
+       ('4', 'Ikigai', 'Francesc Miralles', 300, 60),
+       ('5', 'Deep Work', 'Cal Newport', 500, 20);
+
+       --Display all books that cost less than 450 and have stock more than 30.
+
+
+       --Update the stock to 45 and reduce the price to 420 for the book titled ‘Deep Work’.
+--Delete the book titled ‘Ikigai’.
+--Show the average book price and total number of books currently in the table.
+--Display the top 3 most expensive books available.
