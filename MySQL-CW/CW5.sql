44de37f73c468bb6374bcd86ccadb068b09fc07c
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-11 10:50:36 +0530

    cw

---
+++ MySQL-CW/CW5.sql
@@ -0,0 +1,23 @@
+INSERT INTO `mob_store`(`id`, `brand`, `model`, `price`, `stock`)
+VALUES (1, 'Samsung', 'Galaxy M14', 12000, 30),
+       (2, 'Redmi', 'Note 12', 15000, 25),
+       (3, 'Realme', 'Narzo 50', 13000, 20),
+       (4, 'Samsung', 'Galaxy A23', 18000, 10)
+  
+
+
+--Display all mobiles that cost more than 13000 or have stock less than 15.
+
+--Increase the stock by 5 and update the price to 12500 for the mobile with model = 'Narzo 50'.
+
+
+--Delete the mobile whose id is 2.
+
+
+--Find the lowest and highest price in the table.
+
+
+--Find the total stock of all mobiles in the table.
+
+
+--Show the top 2 most expensive mobiles
--- MySQL-HW/HW4.sql
+++ MySQL-HW/HW4.sql
@@ -6,9 +6,24 @@ VALUES ('1', 'The Alchemist', 'Paulo Coelho', 350, 50),
        ('5', 'Deep Work', 'Cal Newport', 500, 20);
 
        --Display all books that cost less than 450 and have stock more than 30.
-
+        SELECT * FROM `book_database` 
+        WHERE price<'450' AND stock>'40';
 
        --Update the stock to 45 and reduce the price to 420 for the book titled ‘Deep Work’.
+       UPDATE `book_database` 
+       SET stock='45',price='420'
+       WHERE title='Deep Work';
+
+
 --Delete the book titled ‘Ikigai’.
+        DELETE FROM `book_database` 
+        WHERE title='Ikigai';
+
 --Show the average book price and total number of books currently in the table.
+   SELECT 
+   AVG(price) AS average_price,
+    COUNT(*) AS total_books
+FROM `book_database` ;
+
+
 --Display the top 3 most expensive books available.
