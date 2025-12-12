1d750aa7f7f01f8db8e9f8a2a684d940c83b1fab
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-12 23:11:42 +0530

    hw

---
+++ MySQL-HW/HW-6.sql
@@ -0,0 +1,16 @@
+INSERT INTO `user_perfomance`(`id`, `name`, `city`, `score`, `bonus`, `challenge`) 
+VALUES(1, 'Raj', 'Chennai', 88, 5, 'Fitness'),
+(2, 'Anu', 'Mumbai', 91, NULL, 'Diet'),
+(3, 'Ravi', 'Chennai', 78, 3, 'Fitness'),
+(4, 'Meena', 'Delhi', 82, NULL, 'Diet'),
+(5, 'Farah', 'Mumbai', 95, 4, 'Fitness'),
+(6, 'Kiran', 'Pune', 70, NULL, 'Yoga'),
+(7, 'Latha', 'Pune', 87, NULL, 'Fitness');
+
+--Show the details of users whose score is greater than the average score across all users.
+
+
+
+
+--Display the names of users who are participating in the same challenge as the user named 'Farah'.
+
