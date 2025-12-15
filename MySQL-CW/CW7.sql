f36ecb2105f4288a3f5a40645d7cc3752732f0fc
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-15 10:59:15 +0530

    mysql

---
+++ MySQL-CW/CW7.sql
@@ -0,0 +1,26 @@
+--Create a new database called GroceryShop.
+CREATE DATABASE grocery_shop;
+
+
+
+--Use that database.
+--Create a table named products with the following details:
+--product_id (number, should be the main identifier)
+--product_name (text)
+--price (number)
+CREATE TABLE product(
+product_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(20) NOT NULL,
+price INT NOT NULL,
+PRIMARY KEY (product_id));
+
+
+
+--Later, you are asked to add a new column called category (text) to the same table.
+ALTER TABLE product
+ADD category VARCHAR(20);
+
+--the shop owner wants to remove all items from the products table without deleting the table itself.
+DELETE FROM product product_id,product_name,price,category;
+
+--Finally, delete the entire database as it’s no longer needed.
