9a048425bce316a9b3c0da8d407424906cde140f
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-16 10:35:58 +0530

    hw

---
+++ MySQL-HW/HW9.sql
@@ -0,0 +1,16 @@
+--for two tables:employees -departments 
+--Show a list of all employees along with their department name. If an employee doesn’t belong to any department, still show their name with “NULL” for the department.
+--Show only those employees who have a department assigned.
+--how all department assignments, even if the employee doesn’t exist in the employee table-- employees table
+id | name
+---|--------
+1  | Anjali
+2  | Rohan
+3  | Meena
+
+-- departments table
+emp_id | department_name
+--------   |-----------------
+1           | HR
+2           | IT
+4           | Finance
