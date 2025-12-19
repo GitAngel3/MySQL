40367c2b564ea8ff712d7e4eaf3be691d17ff2f5
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-19 10:07:10 +0530

    mysql cw

--- MySQL-CW/CW9.sql
+++ MySQL-CW/CW9.sql
@@ -18,10 +18,34 @@ SELECT books.book_id, books.title, borrowers.name AS borrower_name;
 
 
 --Show the list of all borrowers along with the book details they borrowed (if any).
+SELECT 
+    borrowers.borrower_id,
+    borrowers.name,
+    books.book_id,
+    books.title
+FROM borrowers borrowers
+LEFT JOIN books books
+ON borrowers.book_id = books.book_id;
+
+
 
 --Show the list of all books that have not been borrowed by anyone.
+SELECT 
+    books.book_id,
+    books.title
+FROM books books
+LEFT JOIN borrowers borrowers
+ON books.book_id = borrowers.book_id
+WHERE borrowers.book_id IS NULL;
 
 --Show the list of all borrowers even if they haven't borrowed any book.
+SELECT 
+    borrowers.borrower_id,
+    borrowers.name,
+    books.title
+FROM borrowers borrowers
+LEFT JOIN books books
+ON borrowers.book_id = books.book_id;
 
 
 
